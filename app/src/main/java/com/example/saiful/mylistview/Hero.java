cc90b9cf9ad361ba6da866e53ede956d101dd2eb
MustIpunk <rembangflasher@gmail.com>
2019-06-30 17:02:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/saiful/mylistview/Hero.java
@@ -0,0 +1,31 @@
+package com.example.saiful.mylistview;
+
+public class Hero {
+    private int photo;
+    private String name;
+    private String descripton;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+}
